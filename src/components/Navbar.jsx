ebe42b0de2a11d13aded7fadf44ed4ccb6ca17d3
John Soundrapandian <pandian1045@gmail.com>
2026-07-06 23:27:12 +0530

    second

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -73,7 +73,7 @@ export default function Navbar() {
             }}
           >
             <span className={`method-tag ${link.method.toLowerCase()}`}>--</span>
-            {link.path}
+            {link.label}
           </button>
         ))}
       </div>
